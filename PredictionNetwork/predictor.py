537cca04a7fc0cc51e00e944a009c0fee1b9c9bc
ajskkjdncijansifna <248266816j@gmail.com>
2019-08-17 12:02:12 -0700

    added training data

--- PredictionNetwork/predictor.py
+++ PredictionNetwork/predictor.py
@@ -26,16 +26,22 @@ state_bubble = 0
 
 
 def main():
-    Button(root, text="Quit", command=root.quit).pack()
+    input, answer = load_data("TrainingData.csv")
+    model = build_model()
+    model.fit(input, answer, epochs=150, batch_size=10)
 
+    _, accuracy = model.evaluate(input, answer)
+    print('Accuracy: %.2f' % (accuracy*100))
+
+    Button(root, text="Quit", command=root.quit).pack()
     root.after(0, place_circle)
     root.mainloop()
 
 
-def displayThings(rgb, answer):
+def display_answer(rgb, answer):
     global circleX
     if answer == 0:
-        canvas.move(circle, 200-circleX,0)
+        canvas.move(circle, 200-circleX, 0)
         circleX = 200
     elif answer == 1:
         canvas.move(circle, 300 - circleX, 0)
@@ -46,9 +52,26 @@ def displayThings(rgb, answer):
 
 def place_circle():
     global state_bubble
-    displayThings((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), state_bubble)
+    display_answer((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), state_bubble)
     state_bubble = 0 if state_bubble == 1 else 1
     root.after(1000, place_circle)
+
+
+def load_data(file_name):
+    initial_data = numpy.genfromtxt(file_name, delimiter=',')
+    training_input = initial_data[:, 0:3]
+    traning_answer = initial_data[:, 3]
+    return training_input, traning_answer  # 0 is input, 1 is answer
+
+
+def build_model():
+    model = Sequential()
+    model.add(Dense(12, input_dim=3, activation='relu'))
+    model.add(Dense(8,activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
 
 if __name__ == "__main__":
